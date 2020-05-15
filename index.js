7eb3df1847acbadbd89186f0f2dac09d22354f49
Ryan O'Sullivan <ryanosullivan360@gmail.com>
2020-05-15 11:30:38 -0300

    clean up structure

--- index.js
+++ index.js
@@ -3,18 +3,4 @@ const wallet = require("./wallets");
 const market = require("./market");
 const order = require("./orders");
 
-
-// async function getMarketInfo() {
-//     // rtn = await wallet.allWalletInfo()
-//     // rtn = await market.marketList()
-//     // rtn = await wallet.coinTransactions("CAD",0,100)
-//     let startTime = new Date()
-//     startTime = startTime.getTime() - (7*24 * 60 * 60 * 1000)
-//     rtn = await market.marketData("BTC-CAD", startTime, new Date().getTime(), 60)
-//     fs.appendFile('btc.json',JSON.stringify(rtn),(err)=> {
-//         console.log(err)
-//     })
-// }
-
-// getMarketInfo()
 module.exports = { wallet, market, order, "init": comms.init}
